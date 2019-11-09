3308c6f4ceff5096177055c545e591d8625720f8
Dev Ryu <eostmghf01@gmail.com>
2019-11-09 23:59:58 +0900

    make product

--- eigthTon_shop/settings.py
+++ eigthTon_shop/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user.apps.UserConfig',
+    # 'user.apps.UserConfig',
+    'product',
+
 ]
 
 MIDDLEWARE = [
