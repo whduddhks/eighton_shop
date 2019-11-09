996930ee7d97786eac0bdcebc4259899c6feb387
whduddhks <39693818+whduddhks@users.noreply.github.com>
2019-11-10 01:05:13 +0900

    url수정

--- eigthTon_shop/urls.py
+++ eigthTon_shop/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import user.views
+import product.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
+    path('', include('product.urls')),
 ]
