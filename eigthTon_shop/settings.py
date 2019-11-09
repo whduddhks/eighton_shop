a1a7f4d845a10855da3fa2f2a563a99bc369cedb
whduddhks <39693818+whduddhks@users.noreply.github.com>
2019-11-10 02:50:20 +0900

    로그인 완성

--- eigthTon_shop/settings.py
+++ eigthTon_shop/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'user', 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
